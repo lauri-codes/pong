4b66f0947456b07ba5c6a2ddcb276a1e74623db9
Lauri Himanen <lauri.himanen@aalto.fi>
2018-10-16 19:14:27 +0300

    Don't remember what.

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char *argv[])
 
     // Use the following kind of loop to play advance the game manually with custom input
     // The a.exit closes the application
-    std::vector<float> input = {0, 1};
-    for (int i = 0; i < 10; ++i) {
-        QApplication::processEvents(QEventLoop::AllEvents, 1000);
-        std::vector<float> output = game.requestPosVel(input);
-        qDebug() << output;
-        QApplication::processEvents(QEventLoop::AllEvents, 1000);
-        QThread::sleep(1);
-    }
-    a.exit();
+//    std::vector<float> input = {0, 1};
+//    for (int i = 0; i < 10; ++i) {
+//        QApplication::processEvents(QEventLoop::AllEvents, 1000);
+//        std::vector<float> output = game.requestPosVel(input);
+//        qDebug() << output;
+//        QApplication::processEvents(QEventLoop::AllEvents, 1000);
+//        QThread::sleep(1);
+//    }
+//    a.exit();
 
     // Use these lines to manullla play the game
-    //game.loop()
-    //a.exec();
+    game.loop();
+    a.exec();
 
 }
