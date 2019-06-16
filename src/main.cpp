7898cb25e680d539a69c062f2dee9a9fadb06cbf
Lauri Himanen <lauri.himanen@aalto.fi>
2019-06-17 00:10:21 +0300

    Initial setup of the neural player.

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,30 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Game game;
 
+    // This section uses a NN to play the game
+
     // Initialize the NN
     auto net = std::make_shared<Net>();
-    //torch::Tensor tensor = torch::rand({2, 3});
-    //std::cout << tensor << std::endl;
 
     // Use the following kind of loop to play advance the game manually with custom input
     // The a.exit closes the application
-//    std::vector<float> input = {0, 1};
-//    for (int i = 0; i < 10; ++i) {
-//        QApplication::processEvents(QEventLoop::AllEvents, 1000);
-//        std::vector<float> output = game.requestPosVel(input);
-//        qDebug() << output;
-//        QApplication::processEvents(QEventLoop::AllEvents, 1000);
-//        QThread::sleep(1);
-//    }
-//    a.exit();
+    int sleep = 100;
+    for (int i = 0; i < 100; ++i) {
+        QApplication::processEvents(QEventLoop::AllEvents, sleep);
+
+        // Play one frame of game with network
+        torch::Tensor randomInput = torch::randn({1, 12});
+        auto gameInput = net->forward(randomInput);
+        std::vector<float> gameInputStd(gameInput.data<float>(), gameInput.data<float>() + gameInput.numel());
+        std::cout << gameInputStd << std::endl;
+        auto gameOutput = game.getInput(gameInputStd);
+        auto fitness = game.getFitness();
+
+        QThread::msleep(sleep);
+    }
+    a.exit();
 
     // Use these lines to manually play the game
-    game.loop();
-    a.exec();
+    //game.loop();
+    //a.exec();
 }
--- src/nn.h
+++ src/nn.h
@@ -7,18 +7,17 @@
 struct Net : torch::nn::Module {
   Net() {
     // Construct and register two Linear submodules.
-    fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-    fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-    fc3 = register_module("fc3", torch::nn::Linear(32, 10));
+    fc1 = register_module("fc1", torch::nn::Linear(12, 32));
+    fc2 = register_module("fc2", torch::nn::Linear(32, 16));
+    fc3 = register_module("fc3", torch::nn::Linear(16, 2));
   }
 
-  // Implement the Net's algorithm.
+  // Implement the Net's forward pass.
   torch::Tensor forward(torch::Tensor x) {
-    // Use one of many tensor manipulation functions.
-    x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-    x = torch::dropout(x, /*p=*/0.5, /*train=*/is_training());
+    x = torch::relu(fc1->forward(x));
     x = torch::relu(fc2->forward(x));
-    x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
+    x = torch::relu(fc3->forward(x));
+    x = torch::log_softmax(x, 1);
     return x;
   }
 
